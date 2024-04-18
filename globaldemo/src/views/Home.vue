b6323e70048a235f5fe15ea26ba0fcd95acd5bd3
yanxu1234 <1309678235@qq.com>
2024-04-18 19:57:11 +0800

    还剩后端 前端数据管理 图表 界面美化  离线地图

--- globaldemo/src/views/Home.vue
+++ globaldemo/src/views/Home.vue
@@ -69,6 +69,7 @@ const currentPage1 = ref(5)
 const currentPage2 = ref(5)
 const currentPage3 = ref(5)
 const currentPage4 = ref(1)
+
 const pageSize2 = ref(100)
 const pageSize3 = ref(100)
 const pageSize4 = ref(100)
